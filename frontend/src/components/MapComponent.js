11bce3f58f19a1b770f7070142d515c8891fcfa7
KyleHYX <60859865+KyleHYX@users.noreply.github.com>
2024-02-14 18:32:06 -0800

    tmp changes for retrieving lat and lng on map click

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -1,7 +1,30 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader, Polyline, Marker, OverlayView } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, useJsApiLoader, Polyline, OverlayView } from '@react-google-maps/api';
 
 const MapComponent = ({ polylineData, markerData }) => {
+  // ORI/DST
+  const [locMode, setLocMode] = useState('');
+  const [oriLoc, setOriLoc] = useState(null);
+  const [dstLoc, setDstLoc] = useState(null);
+
+  const onMapClick = (e) => {
+    const loc = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lat(),
+    };
+
+    if (locMode === 'ORI') {
+      setOriLoc(loc);
+      setLocMode('');
+    } else if (locMode === 'DST') {
+      setDstLoc(loc);
+      setLocMode('');
+    }
+  };
+
+  const setLocModeOri = () => setLocMode('ORI')
+  const setLocModeDst = () => setLocMode('DST')
+
   const CustomMarker = ({ position, iconUrl, text }) => {
     const markerStyle = {
       position: 'absolute',
@@ -27,6 +50,7 @@ const MapComponent = ({ polylineData, markerData }) => {
       </OverlayView>
     );
   };
+
   const containerStyle = {
     width: '100%',
     height: '100%',
@@ -47,38 +71,60 @@ const MapComponent = ({ polylineData, markerData }) => {
     strokeWeight: 2,
   };
 
+  const barIndicateSytle = {
+    height: '10px',
+    backgroundColor: 'orange',
+    width: '100%',
+    position: 'absolute',
+    zIndex: 10,
+  };
+
+  const modeButtonStyle = {
+    position: 'absolute',
+    zIndex: 10,
+  }
+
   const { isLoaded } = useJsApiLoader({ id: 'google-map-script', googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY })
 
-  const [map, setMap] = React.useState(null)
+  return (
+    <div>
+      {locMode != '' ? <div style={barIndicateSytle}></div> : <></>}
+      {oriLoc != null ? <div style={barIndicateSytle}>{oriLoc.lat}{oriLoc.lng}</div> : <></>}
+      <div>
+        <button style={modeButtonStyle} onClick={setLocModeOri}>Set Origin</button>
+        <button stype={modeButtonStyle} onClick={setLocModeDst}>Set Destination {dstLoc}</button>
+      </div>
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onClick={onMapClick}
+          >
+            <Polyline
+              path={polylineData}
+              options={polylineOptions}
+            />
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-    >
-      <Polyline
-        path={polylineData}
-        options={polylineOptions}
-      />
+            {markerData.map((markerPosition, index) => {
+              let iconUrl;
+              if (index === 0) iconUrl = "https://maps.gstatic.com/mapfiles/ms2/micons/blue.png";
+              else if (index === markerData.length - 1) iconUrl = "http://maps.gstatic.com/mapfiles/ms2/micons/pink.png";
+              else iconUrl = "http://maps.gstatic.com/mapfiles/ms2/micons/cycling.png";
 
-      {markerData.map((markerPosition, index) => {
-        let iconUrl;
-        if (index === 0) iconUrl = "https://maps.gstatic.com/mapfiles/ms2/micons/blue.png";
-        else if (index === markerData.length - 1) iconUrl = "http://maps.gstatic.com/mapfiles/ms2/micons/pink.png";
-        else iconUrl = "http://maps.gstatic.com/mapfiles/ms2/micons/cycling.png";
-
-        return (
-          <CustomMarker
-            key={index}
-            position={markerPosition.pos}
-            iconUrl={iconUrl}
-            text={markerPosition.station}
-          />
-        );
-      })}
-    </GoogleMap>
-  ) : <></>
+              return (
+                <CustomMarker
+                  key={index}
+                  position={markerPosition.pos}
+                  iconUrl={iconUrl}
+                  text={markerPosition.station}
+                />
+              );
+            })}
+          </GoogleMap>
+        ) : <></>}
+        </div>
+  );
 };
 
 export default React.memo(MapComponent);
